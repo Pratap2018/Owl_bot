f0d9cba535254566b8cd0e0a6e4b47bff7520659
Pratap <pratapmridha@gmail.com>
2020-10-04 12:09:18 +0530

    Fixed Bug

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 
 client.once('ready', () => {
-    //console.log("Ready!!!");
+    console.log("Ready!!!");
 });
 
 client.on('message', message => {
